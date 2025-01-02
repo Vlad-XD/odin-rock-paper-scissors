d9d2174f8b2d6d9ad480c510e5a02e2315f2ca20
Vlad-XD <192461579+Vlad-XD@users.noreply.github.com>
2025-01-01 17:43:03 -0800

    Implement logic to play single round

--- script.js
+++ script.js
@@ -1,5 +1,71 @@
-console.log(`The computer plays... ${getComputerChoice()}!`);
-console.log(`The user plays... ${getHumanChoice()}! `);
+// player/computer scores
+let humanScore = 0;
+let computerScore = 0;
+
+
+while (humanScore < 1) {
+  playRound(getHumanChoice(), getComputerChoice());
+}
+
+/*
+  playRound: takes human and computer player choices as arguments,
+             increases corresponding score depending on who wins game,
+             and returns a message on the console.
+  Parameters: string humanChoice, string computerChoice
+  Return value: N/A
+*/
+function playRound(humanChoice, computerChoice) {
+  switch (humanChoice) {
+    case "rock" :
+      switch (computerChoice) {
+        case "rock" :
+          console.log("It is a tie!");
+          break;
+        case "paper" :
+          console.log("You lose! Paper beats Rock!");
+          computerScore++;
+          break;
+        case "scissors" :
+          console.log("You win! Rock beats Scissors!");
+          humanScore++;
+          break;
+      }
+      break;
+
+    case "paper" :
+      switch (computerChoice) {
+        case "rock" :
+          console.log("You win! Paper beats Rock!");
+          humanScore++;
+          break;
+        case "paper" :
+          console.log("It is a tie!");
+          break;
+        case "scissors" :
+          console.log("You lose! Scissors beat Paper!");
+          computerScore++;
+          break;
+      }      
+      break;
+
+    case "scissors" :
+      switch (computerChoice) {
+        case "rock" :
+          console.log("You lose! Rock beats Scissors!");
+          computerScore++;
+          break;
+        case "paper" :
+          console.log("You win! Scissors beat Paper!");
+          humanScore++;
+          break;
+        case "scissors" :
+          console.log("It is a tie!");
+          break;
+      }          
+      break;
+  }
+
+}
 
 /*
   getComputerChoice: randomly returns one of the following strings:
@@ -39,7 +105,7 @@ function getHumanChoice() {
   // loop until valid choice is entered in prompt
   while (needHumanChoice) {
     // prompt user
-    choice = prompt(PROMPT_MESSAGE);
+    choice = prompt(PROMPT_MESSAGE).toLowerCase();
 
     // validate input
     if (choice === "rock" || choice === "paper" || choice === "scissors") {
