c4be70c3ec2756edd33dc17f3b416094cca0a970
Vlad-XD <192461579+Vlad-XD@users.noreply.github.com>
2025-01-01 17:16:57 -0800

    Implement human choice logic

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 console.log(`The computer plays... ${getComputerChoice()}!`);
+console.log(`The user plays... ${getHumanChoice()}! `);
 
 /*
   getComputerChoice: randomly returns one of the following strings:
-                      "rock", "paper", "scissors"
+                      "rock", "paper", "scissors".
   Parameters: N/A
   Return value: string
 */
@@ -18,6 +19,34 @@ function getComputerChoice() {
   else {
     choice = (randomNum >= 0.66) ? "paper" : "scissors"; 
   }
-  
+
+  return choice;
+}
+
+/*
+  getHumanChoice: prompt user for choice and validate it is either
+                    "rock", "paper", or "scissors".
+  Parameters: N/A
+  Return value: string
+*/
+
+function getHumanChoice() {
+  // variable initilizations
+  const PROMPT_MESSAGE = "Enter a choice between rock, paper, or scissors:";
+  let choice = "";
+  let needHumanChoice = true;
+
+  // loop until valid choice is entered in prompt
+  while (needHumanChoice) {
+    // prompt user
+    choice = prompt(PROMPT_MESSAGE);
+
+    // validate input
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+      needHumanChoice = false;
+    }
+
+  }
+
   return choice;
 }
