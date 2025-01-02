0603dd5d2c8cc4bfacb9e3e600d61c7f381b00ee
Vlad-XD <192461579+Vlad-XD@users.noreply.github.com>
2025-01-01 17:02:35 -0800

    Implement computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+console.log(`The computer plays... ${getComputerChoice()}!`);
+
+/*
+  getComputerChoice: randomly returns one of the following strings:
+                      "rock", "paper", "scissors"
+  Parameters: N/A
+  Return value: string
+*/
+
+function getComputerChoice() {
+  // variable initializations
+  let choice = "";
+  let randomNum = Math.random();
+
+  if (randomNum <= 0.33){
+    choice = "rock";
+  }
+  else {
+    choice = (randomNum >= 0.66) ? "paper" : "scissors"; 
+  }
+  
+  return choice;
+}
